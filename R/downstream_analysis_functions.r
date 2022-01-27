0a3aaae1457dc170ae0a698abded9ce004db59a1
MattPM <mattmule@gmail.com>
2022-01-27 13:39:57 +0000

    Add back RunFgseaOnRankList function to maintain back compatibility with R3.6 version (used in thymoma).

--- R/downstream_analysis_functions.r
+++ R/downstream_analysis_functions.r
@@ -235,7 +235,7 @@ FgseaList = function(..., rank.list.celltype, pathways,
   gsea = lapply(rank.list.celltype, function(x){
     fgsea::fgsea(...,
                  pathways = pathways,
-                 stats = rank.list.celltype[[i]],
+                 stats = x,
                  maxSize = maxSize,
                  minSize = minSize) %>%
       dplyr::mutate(celltype = names(rank.list.celltype)[i]) %>%
@@ -244,6 +244,51 @@ FgseaList = function(..., rank.list.celltype, pathways,
   names(gsea) = names(rank.list.celltype)
   return(gsea)
 }
+
+
+#' RunFgseaOnRankList - wrapper around fast gene set enrichment analysis with the fgsea R package https://bioconductor.org/packages/release/bioc/html/fgsea.html
+#'
+#' @param rank.list.celltype results returned by GetRankResultsRaw or GetRankResults
+#' @param pathways modules / gene sets as a named list each a single vector of unique gene IDS
+#' @param maxSize see fgsea package
+#' @param minSize see fgsea package
+#' @param nperm recommended to keep set at 25000 based on optomization and p value stabilization
+#' @param positive.enrich.only include negative enrichments in results? TRUE/FALSE
+#'
+#' @return
+#' @importFrom fgsea fgsea
+#' @importFrom dplyr arrange filter
+#' @export
+#'
+#' @examples
+#' # usage:
+#' t1hvl_rank = GetRankResultsRaw(limma.fit.object.list  = ebf, coefficient.number = 1, contrast.name = "time_1_highvslow")
+#' gsea = RunFgseaOnRankList(rank.list.celltype = t1hvl_rank, )
+RunFgseaOnRankList = function(rank.list.celltype, pathways, maxSize = 500, minSize = 9,
+                              nperm = 250000, positive.enrich.only = FALSE) {
+
+  .Deprecated("FgseaList")
+
+  #require(fgsea)
+  gsea = list()
+  for (i in 1:length(rank.list.celltype)) {
+    gsea[[i]] =
+      suppressMessages(fgsea::fgsea(pathways = pathways,
+                                    stats = rank.list.celltype[[i]],
+                                    maxSize = maxSize,
+                                    minSize = minSize,
+                                    nperm = nperm)) %>%
+      dplyr::mutate(celltype = names(rank.list.celltype)[i]) %>%
+      dplyr::arrange(pval)
+    if (positive.enrich.only == TRUE) {
+      gsea[[i]] = gsea[[i]] %>% dplyr::filter(ES > 0)
+    }
+  }
+  names(gsea) = names(rank.list.celltype)
+  return(gsea)
+}
+
+
 
 
 #' RbindGseaResultList - prepare gsea result list for visualization funcitons GseaBubblePlot or GseaBarPlot
